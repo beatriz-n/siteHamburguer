7c07fc78b24ce5ebb12346cc1a2443dbe58ebed5
beatriz <beatriznespolid@gmail.com>
2022-08-05 14:48:56 -0300

    create

---
+++ prod/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  <router-view/>
+  <p>sdad</p>
+  </div>
+</template>
+
+<style>
+</style>
